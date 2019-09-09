8c9235f57ad9029375db6d03adab38dee312488f
max667max <maksim.niechaiev@mail.ru>
2019-09-09 12:49:59 +0800

    refract

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 
-void main () => runApp(MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: LayoutBuilder(
-        builder: (context, constrain){
-          if (constrain.maxWidth < 1000){
+        builder: (context, constrain) {
+          if (constrain.maxWidth < 1000) {
             return Mobyle();
-          } 
+          }
           return Desctop();
         },
       ),
@@ -22,16 +22,28 @@ class Mobyle extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: TextField(onChanged: (s){print(s);}, decoration: InputDecoration(labelText: "lable", prefix: Icon(Icons.ac_unit), suffixIcon: Icon(Icons.check_circle_outline), )),),
-
+      body: Center(
+        child: TextField(
+            onChanged: (s) {
+              print(s);
+            },
+            decoration: InputDecoration(
+              labelText: "lable",
+              prefix: Icon(Icons.ac_unit),
+              suffixIcon: Icon(Icons.check_circle_outline),
+            )),
+      ),
     );
   }
 }
+
 class Desctop extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Desctop"),),
+      body: Center(
+        child: Text("Desctop"),
+      ),
     );
   }
 }
